1b77853caa80edfdbd3e9ff6935e82ec8854b6fd
ykotsiuba <jurok3x@gmail.com>
2022-01-28 22:56:13 +0200

    Added announcement and comment forms

--- src/app/view-classroom/view-classroom.component.ts
+++ src/app/view-classroom/view-classroom.component.ts
@@ -1,14 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { environment } from 'src/environments/environment';
-import { Announcement } from '../model/announcement';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
 import { Classroom } from '../model/classroom';
 import { User } from '../model/user';
-import { Comments } from '../model/comment';
 import { ClassroomService } from '../service/classroom.service';
 import { CommentService } from '../service/comment.service';
 import { AnnouncementService } from '../service/announcement.service';
-import { Observable, take } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 
 
@@ -20,17 +17,12 @@ import { ActivatedRoute } from '@angular/router';
 export class ViewClassroomComponent implements OnInit {
 
   students$!: Observable<User[]>;
+
   teachers$!: Observable<User[]>;
+
   owner$!: Observable<User>;
-  classroom: Classroom | undefined;
+  
   classroom$!: Observable<Classroom>;
-  announcements: Announcement[] | undefined;
-  announcementForm: FormGroup = this.formBuilder.group({
-    text: ''
-  });
-  commentForm: FormGroup = this.formBuilder.group({
-    text: ''
-  });
 
   constructor(private classroomService: ClassroomService,
               private commentService: CommentService,
@@ -47,34 +39,10 @@ export class ViewClassroomComponent implements OnInit {
     this. teachers$ = this.classroomService.getClassroomUsers(id, 'teachers');
     this.students$ = this.classroomService.getClassroomUsers(id, 'students');
     this.owner$ = this.classroomService.getClassroomOwner(id);
-    this.classroom = JSON.parse(localStorage.getItem(environment.classroom) || '');
-    this.announcementService.getAnnouncementsByClassroom(id).pipe(take(1)).subscribe(
-      (response: Announcement[]) => {
-        this.announcements = response;
-        response.forEach(ann =>
-          this.commentService.getCommentsByAnnouncement(ann.id).pipe(take(1)).subscribe(
-            (response2: Comments[]) => ann.comments = response2))
-      });
   }
 
   readLocalStorageValue(key: string) {
     return localStorage.getItem(key);
   }
-
-  sendComment(announcement: Announcement){
-    this.classroom = JSON.parse(localStorage.getItem(environment.classroom) || '');
-    let comment = new Comments();
-    comment.text = this.commentForm.get(['text'])?.value;
-    comment.announcementId = announcement.id;
-    let author: User = JSON.parse(localStorage.getItem(environment.user) || '')
-    comment.authorId = author.id;
-    this.commentService.createComment(comment, comment.authorId);
-  }
-
-  deleteComment(comment: Comments){
-    this.commentService.deleteComment(comment.id);
-  }
-
-  
 
 }
