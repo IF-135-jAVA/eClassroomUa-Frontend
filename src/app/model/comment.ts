4edab629cfd8fa2bb20a980dbdb6f6d0f438ed13
Oksana <oksanamow@gmail.com>
2022-02-17 16:53:58 +0200

    add delete and update methods to announcement.component and comment-details.component

--- src/app/model/comment.ts
+++ src/app/model/comment.ts
@@ -1,13 +1,15 @@
 export class Comments{
     id: number = 0;
     text: string = "";
+    firstName: string = "";
+    lastName: string = "";
     date: Date = new Date();
     authorId: number = 0;
     announcementId: number = 0;
     materialId: number = 0;
     userAssignmentId: number = 0;
     enabled: boolean = true;
-    
+
     constructor(values: object = {}) {
         Object.assign(this, values);
     }
