e1eb2eed732ffd8a5e77771f0aa3b589954760d3
Khrystyna12 <Khrystynka.Sem@gmail.com>
2022-02-19 00:15:13 +0200

    Fixed components user-assignments and user-assignment-details

--- src/app/model/user-assignment.ts
+++ src/app/model/user-assignment.ts
@@ -1,13 +1,19 @@
 export class UserAssignment{
     id: number = 0;
     materialId: number = 0;
+    materialTitle: string = "";
+    dueDate: Date = new Date();
+    maxScore: number = 0;
     userId: number = 0;
+    userFirstName: string = "";
+    userLastName: string = "";
     assignmentStatusId: number = 0;
+    assignmentStatusTitle: string = "";
+    submissionDate: Date = new Date();
     grade: number = 0;
     feedback: string = "";
-    submissionDate: Date = new Date();
     enabled: boolean = true;
-    
+
     constructor(values: object = {}) {
         Object.assign(this, values);
     }
--- src/app/user-assignment-details/user-assignment-details.component.ts
+++ src/app/user-assignment-details/user-assignment-details.component.ts
@@ -2,13 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {UserAssignment} from "../model/user-assignment";
 import {UserAssignmentService} from "../service/user-assignment.service";
 import {ActivatedRoute, Router} from "@angular/router";
-import {Topic} from "../model/topic";
-import {Material} from "../model/material";
 import {formatDate} from "@angular/common";
-import {AssignmentStatus} from "../model/assignment-status";
-import {User} from "../model/user";
-import {UserService} from "../service/user.service";
-import {MaterialService} from "../service/material.service";
 import {Answer} from "../model/answer";
 import {AnswerService} from "../service/answer.service";
 import {JwtHelperService} from "@auth0/angular-jwt";
@@ -25,8 +19,6 @@ export class UserAssignmentDetailsComponent implements OnInit {
   userAssignment!: UserAssignment;
   materialId!: number;
   id!: number;
-  materialTitle!: String;
-  userName!: String;
   answers: Answer[] | undefined;
 
   userRole!: string;
@@ -37,8 +29,6 @@ export class UserAssignmentDetailsComponent implements OnInit {
 
   constructor(
     private userAssignmentService: UserAssignmentService,
-    private userService: UserService,
-    private materialService: MaterialService,
     private answerService: AnswerService,
     private activatedRoute: ActivatedRoute,
     private router: Router,
@@ -51,7 +41,6 @@ export class UserAssignmentDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.getUserAssignment();
-    this.getMaterialTitle();
     this.getAnswers();
   }
 
@@ -59,33 +48,12 @@ export class UserAssignmentDetailsComponent implements OnInit {
     this.userAssignmentService.getUserAssignmentById(this.materialId, this.id).subscribe(
       (response: UserAssignment) => {
         this.userAssignment = response;
-        this.getUserName();
-      }
-    )
-  }
-
-  getUserName() {
-    this.userService.getUserById(this.userAssignment.userId).subscribe(
-      (response: User) => {
-        this.userName = response.firstName + " " + response.lastName;
-      }
-    )
-  }
-
-  getMaterialTitle() {
-    this.materialService.getMaterialById(new Topic(), this.materialId).subscribe(
-      (response: Material) => {
-        this.materialTitle = response.title;
       }
     )
   }
 
   getSubmissionDate(userAssignment: UserAssignment): String {
-    return userAssignment.submissionDate == null ? "Not submitted yet" : formatDate(userAssignment.submissionDate, "medium", "en-US");
-  }
-
-  getAssignmentStatus(value: number): String {
-    return AssignmentStatus[value];
+    return userAssignment.submissionDate == null ? "Not submitted yet" : formatDate(userAssignment.submissionDate, "MMM dd, yyyy, HH:mm", "en-US");
   }
 
   getAnswers() {
